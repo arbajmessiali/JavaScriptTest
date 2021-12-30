2cdbd3ccf7ed4cd33f7a4ae60928babe501d923c
arbajmessiali <34254265+arbajmessiali@users.noreply.github.com>
2021-12-30 14:44:48 +0530

    Sprite animation test

--- game.js
+++ game.js
@@ -1,1 +1,26 @@
-console.log("It is running");
+var canvas = document.getElementById("Game");
+var ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+//create empty array with 4 frames
+var images = [];
+images.length = 4;
+
+//fill array with images
+for(var i=1; i<=images.length;i++)
+{
+    images[i] = new Image;
+    images[i].src = 'Ball_'+i.toString()+'.png';
+}
+
+//draw image on canvas
+var i=1;
+setInterval(function(){
+    if(i>4)
+    {
+        i=1;
+    }
+    ctx.drawImage(images[i],100,100,100,100);
+},100);
