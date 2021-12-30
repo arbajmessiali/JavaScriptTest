438e95598bc033cc10535e5dd8463da0e73f1dcb
arbajmessiali <34254265+arbajmessiali@users.noreply.github.com>
2021-12-30 14:53:47 +0530

    sprite animate another try

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById("Game");
+var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
@@ -12,15 +12,16 @@ images.length = 4;
 for(var i=1; i<=images.length;i++)
 {
     images[i] = new Image;
-    images[i].src = 'src/Ball_'+i.toString()+'.png';
+    images[i].src = 'Ball_'+ i.toString() +'.png';
 }
 
 //draw image on canvas
 var i=1;
 setInterval(function(){
+    i++;
     if(i>4)
     {
         i=1;
     }
     ctx.drawImage(images[i],100,100,100,100);
-},100);
+},100)
